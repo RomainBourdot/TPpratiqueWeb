76f1a64aa9710e039299c67720e57e51cb2fffca
romain <romain.bourdot@ynov.com>
2024-10-14 11:01:01 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"text/template"
+)
+
+func main() {
+
+	fileServer := http.FileServer(http.Dir("./assets/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
+	temp, err := template.ParseGlob("./templates/*.html")
+	if err != nil {
+		fmt.Println(fmt.Printf("ERREUR => %v", err.Error()))
+		os.Exit(02)
+	}
+
+	type Etudiant struct {
+		Nom    string
+		Prenom string
+		Age    int
+		Sexe   string
+	}
+
+	type PagePromo struct {
+		NomDeClasse  string
+		Filiere      string
+		Niveau       string
+		NbrEtudiant  int
+		ListEtudiant []Etudiant
+	}
+
+	http.HandleFunc("/promo", func(w http.ResponseWriter, r *http.Request) {
+		etudiant1 := Etudiant{Nom: "Amaru", Prenom: "Tom", Age: 18, Sexe: "Homme"}
+		etudiant2 := Etudiant{Nom: "Coquette", Prenom: "Tomyette", Age: 21, Sexe: "Femme"}
+
+		classe := PagePromo{NomDeClasse: "B1 Informatique", Filiere: "Informatique", Niveau: "B1", NbrEtudiant: 2,
+			ListEtudiant: []Etudiant{etudiant1, etudiant2}}
+
+		temp.ExecuteTemplate(w, "promo", classe)
+	})
+
+	http.ListenAndServe("localhost:8080", nil)
+}
